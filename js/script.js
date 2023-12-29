53e90b969375f87f7c1a776d6ad7577c490d8262
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-29 18:31:11 +0100

    added ul and li inside container

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 console.log('JS OK');
 
+// RECUPERO L'ELEMENTO 
+const element = document.querySelector('.container')
+console.log(element)
+// CREO LISTA
+let listItems = '<ul class="list-unstyled d-flex flex-wrap gap-2">';
+console.log(listItems)
 
 // Creo il ciclo di conteggio
 for( let i= 0 ; i < 100 ; i++){
     if((i+1) % 3 === 0 && (i+1) % 5 === 0){
-        console.log('FizzBuzz');
+        listItems +=`<li class="bg-danger">Fizzbuzz</li>`;
     }
     else if((i+1) % 3 === 0){
-    console.log('Fizz');
+        listItems += `<li class="bg-warning">Fizz</li>`;
+        
     }
     else if((i+1) % 5 === 0 ){
-        console.log('Buzz');
+        listItems += `<li class="bg-success">Buzz</li>`;
     }
     else{
-        console.log(i + 1);
+        listItems += `<li class="bg-primary">${i + 1}</li>`;
     }
 };
 
+listItems += '</ul>';
+element.innerHTML = listItems;
