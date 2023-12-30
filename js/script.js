0b23f941a62468609388f9d4928b3ba735a4da50
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-30 11:13:28 +0100

    added ul element in html

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-console.log('JS OK');
-
 // RECUPERO L'ELEMENTO 
-const element = document.querySelector('.container')
-console.log(element)
-// CREO LISTA
-let listItems = '<ul class="list-unstyled d-flex flex-wrap gap-2">';
-console.log(listItems)
+const listElement = document.getElementById('list');
+console.log(listElement)
+
 
 // Creo il ciclo di conteggio
 for( let i= 0 ; i < 100 ; i++){
+    let content = i
     if((i+1) % 3 === 0 && (i+1) % 5 === 0){
         listItems +=`<li class="bg-danger">Fizzbuzz</li>`;
     }
     else if((i+1) % 3 === 0){
         listItems += `<li class="bg-warning">Fizz</li>`;
-        
     }
     else if((i+1) % 5 === 0 ){
         listItems += `<li class="bg-success">Buzz</li>`;
@@ -23,6 +19,3 @@ for( let i= 0 ; i < 100 ; i++){
         listItems += `<li class="bg-primary">${i + 1}</li>`;
     }
 };
-
-listItems += '</ul>';
-element.innerHTML = listItems;
